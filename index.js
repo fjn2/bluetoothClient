be979f1c2bec2b4e9be2913f84390096ba18f3b9
Federico Naveira <federiconaveira@gmail.com>
2018-05-01 15:25:31 -0300

    Removed unncessary code

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ serial.listPairedDevices(function(pairedDevices) {
   console.log(JSON.stringify(pairedDevices));
     const result = pairedDevices.find((device) => (device.name === 'OBDII'));
     // const service = result.services.find((srv) => (srv.name === 'RFCOMM custom service'));
-    const service = result.services[0];
+    if (result) {
+      const service = result.services[0];
+    }
+
     serial.connect(result.address, service.channel, () =>{
       console.log('connected');
       serial.on('data', function(buffer) {
@@ -14,6 +17,7 @@ serial.listPairedDevices(function(pairedDevices) {
     }, () => {
       console.log('error');
     });
+
 });
 
 var readline = require('readline');
@@ -28,33 +32,3 @@ rl.on('line', function(line){
     if (err) console.log(err);
   });
 })
-
-return;
-
-var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
-
-btSerial.on('found', function(address, name) {
-  console.log(address, name);
-  btSerial.findSerialPortChannel(address, function(channel) {
-    btSerial.connect(address, channel, function() {
-      console.log('connected');
-
-      btSerial.write(new Buffer('my data', 'utf-8'), function(err, bytesWritten) {
-        if (err) console.log(err);
-      });
-
-      btSerial.on('data', function(buffer) {
-        console.log(buffer.toString('utf-8'));
-      });
-    }, function (a,b) {
-      console.log('cannot connect', a, b);
-    });
-
-    // close the connection when you're ready
-    // btSerial.close();
-  }, function() {
-    console.log('found nothing');
-  });
-});
-
-btSerial.inquire();
